d67fa2cc0313ca90cfa5501d30cb81cbd1bc1c6b
Sultepronit <65405818+Sultepronit@users.noreply.github.com>
2021-02-09 21:05:10 +0200

    Add files via upload

---
+++ atoa.js
@@ -0,0 +1,281 @@
+var kan=['','',''];
+var wor = '';
+var buk=['','',''];
+var mit=[0,0,0];
+var aed=0;
+
+var ret0 = '';
+var ret = ['','',''];
+var reti = [0,0,0];
+var reed = 0;
+var reka = ['','',''];
+
+var sps = ['の','ょ','ん','ま','た'];
+var ski = [0,0,0,0,0];
+var ska=5;
+
+function atoa1(aaa)
+{wor=aaa;
+
+//sps=['の','ょ','ん'];
+//sps[0]='の';
+//ski[0]=1;
+//ska=1;
+
+for(x=0;x<999;x++)
+{
+if(wor[x]>"");else{aed=x; break;}
+kan[x]=wor[x];
+	for(y=1;y<999;y++)
+	{
+		if(a_a[y][0]=="end"){buk[x]="/"; break;}
+		if(a_a[y][0]==wor[x]){buk[x]=a_a[y][1]; break;}
+	}
+	
+	for(y=0;y<=ska;y++)
+	{
+		if(wor[x]==sps[y]){ski[y]++; break;}
+		if(y==ska){sps[ska++]=wor[x]; ski[y]=1; break;}
+	}
+}
+mit[0]=0;
+for(x=1;;x++)
+{mit[x]=0;
+	
+	if(x==aed){
+		if(buk[x-1]=='СУ')
+		{
+			for(;;){
+			if(buk[x-2]=='О'){break;}
+			buk[x-1]='С'; mit[x-1]=2; break;
+			}
+		}
+
+		if(wor[x-1]=='。')
+		{
+			if(buk[x-2]=='СУ')//す。
+			{
+			for(;;){
+			if(buk[x-3]=='О'){break;}
+			buk[x-2]='С'; mit[x-2]=2; break;			}
+			}
+			if(wor[x-2]=='か' && wor[x-3]=='　')//すか。
+			{
+				buk[x-4]='С'; mit[x-4]=2;
+			}
+		}
+		
+		break;}
+		
+	if(wor[x]=='ゑ'){kan[x]='へ'; mit[x]=3; continue;}
+	if(wor[x]=='ゐ'){kan[x]='は'; mit[x]=3; continue;}
+	
+	if(buk[x-1]=='ц'){buk[x-1]=buk[x][0];}
+	if(buk[x]=='І')
+	{
+		if(buk[x-1][1]=='Е'){buk[x]='Е'; mit[x]=3; continue;}
+		if(buk[x-1][0]=='Е'){buk[x]='Е'; mit[x]=3; continue;}
+		if(buk[x-1][1]=='А'){buk[x]='Й'; mit[x]=2; continue;}
+		if(buk[x-1][0]=='А'){buk[x]='Й'; mit[x]=2; continue;}
+		if(buk[x-1][1]=='О'){buk[x]='Й'; mit[x]=2; continue;}
+		if(buk[x-1][0]=='О'){buk[x]='Й'; mit[x]=2; continue;}
+		if(buk[x-1][1]=='У'){buk[x]='Й'; mit[x]=2; continue;}
+		if(buk[x-1][0]=='У'){buk[x]='Й'; mit[x]=2; continue;}
+	}
+	
+	if(buk[x]=='У')
+	{
+		if(buk[x-1][1]=='О'){buk[x]='О'; mit[x]=3; continue;}
+	}
+	
+	if(buk[x-1]=='Н')
+	{
+		if(buk[x][0]=='Б'){buk[x-1]='М'; mit[x-1]=1; continue}
+		if(buk[x][0]=='П'){buk[x-1]='М'; mit[x-1]=1; continue}
+		if(buk[x][0]=='М'){buk[x-1]='М'; mit[x-1]=1; continue}
+		if(buk[x][0]=='В'){buk[x-1]='М'; mit[x-1]=1; continue}
+	}
+	
+	if(buk[x]=='ЬО')
+	{
+		if(buk[x-1]=='ҐІ'){buk[x-1]='Ґ'; mit[x-1]=1; mit[x]=1; continue;}
+		if(buk[x-1]=='ЧІ'){buk[x-1]='Ч'; mit[x-1]=1; mit[x]=1; continue;}
+		if(buk[x-1]=='ШІ'){buk[x-1]='Ш'; mit[x-1]=1; mit[x]=1; continue;}
+		if(buk[x-1]=='КІ'){buk[x-1]='К'; mit[x-1]=1; mit[x]=1; continue;}
+		if(buk[x-1]=='РІ'){buk[x-1]='Р'; mit[x-1]=1; mit[x]=1; continue;}
+		if(buk[x-1]=='ДЖІ'){buk[x-1]='ДЖ'; mit[x-1]=1; mit[x]=1; continue;}
+	}
+	if(buk[x]=='ю')
+	{buk[x]='Ю';
+		if(buk[x-1]=='ҐІ'){buk[x-1]='Ґ'; mit[x-1]=1; mit[x]=1; continue;}
+		if(buk[x-1]=='ЧІ'){buk[x-1]='Ч'; mit[x-1]=1; mit[x]=1; continue;}
+		if(buk[x-1]=='ШІ'){buk[x-1]='Ш'; mit[x-1]=1; mit[x]=1; continue;}
+		if(buk[x-1]=='КІ'){buk[x-1]='К'; mit[x-1]=1; mit[x]=1; continue;}
+		if(buk[x-1]=='РІ'){buk[x-1]='Р'; mit[x-1]=1; mit[x]=1; continue;}
+		if(buk[x-1]=='ДЖІ'){buk[x-1]='ДЖ'; mit[x-1]=1; mit[x]=1; continue;}
+	}
+	if(buk[x]=='я')
+	{buk[x]='Я';
+		if(buk[x-1]=='ҐІ'){buk[x-1]='Ґ'; mit[x-1]=1; mit[x]=1; continue;}
+		if(buk[x-1]=='ЧІ'){buk[x-1]='Ч'; mit[x-1]=1; mit[x]=1; continue;}
+		if(buk[x-1]=='ШІ'){buk[x-1]='Ш'; mit[x-1]=1; mit[x]=1; continue;}
+		if(buk[x-1]=='КІ'){buk[x-1]='К'; mit[x-1]=1; mit[x]=1; continue;}
+		if(buk[x-1]=='РІ'){buk[x-1]='Р'; mit[x-1]=1; mit[x]=1; continue;}
+		if(buk[x-1]=='ДЖІ'){buk[x-1]='ДЖ'; mit[x-1]=1; mit[x]=1; continue;}
+	}
+	
+	if(buk[x-1]=='КУ')
+	{
+		if(buk[x][0]=='С'){buk[x-1]='К'; mit[x-1]=2; continue;}
+		if(buk[x][0]=='Ч'){buk[x-1]='К'; mit[x-1]=2; continue;}
+		if(buk[x][0]=='Ш'){buk[x-1]='К'; mit[x-1]=2; continue;}
+		if(buk[x][0]=='Т'){buk[x-1]='К'; mit[x-1]=2; continue;}
+	}
+	if(buk[x-1]=='ШІ')
+	{
+		if(buk[x][0]=='Ч'){buk[x-1]='ШЬ'; mit[x-1]=2; continue;}
+		if(buk[x][0]=='Т'){buk[x-1]='ШЬ'; mit[x-1]=2; continue;}
+		if(buk[x][0]=='К'){buk[x-1]='ШЬ'; mit[x-1]=2; continue;}
+	}
+	if(buk[x-1]=='ЦУ')
+	{
+		if(buk[x][0]=='К'){buk[x-1]='Ц'; mit[x-1]=2; continue;}
+	}
+}
+
+return out1();
+}
+
+function out1()
+{
+var oou = "<div class='js'>";
+for(x=0;x<aed;x++)
+{
+	oou+="<div class='jw'>";
+	if(mit[x]==0){
+	oou+="<p class='trs'>"+buk[x]+"</p>";
+	oou+="<p class='jj'>"+wor[x]+"</p>";}
+	else{
+	if(mit[x]==3){
+	oou+="<p class='trs'><span class='red'>"+buk[x]+"</span></p>";
+	oou+="<p class='jj'><span class='red'>"+kan[x]+"</span></p>";	}
+	if(mit[x]==1){
+	oou+="<p class='trs'><span class='zel'>"+buk[x]+"</span></p>";
+	oou+="<p class='jj'><span class='zel'>"+wor[x]+"</span></p>";	}
+	if(mit[x]==2){
+	oou+="<p class='trs'><span class='blu'>"+buk[x]+"</span></p>";
+	oou+="<p class='jj'><span class='blu'>"+wor[x]+"</span></p>";	}
+	}
+	//oou+="<p class='jj'>";
+	oou+="</div>";
+}
+oou += "</div>";
+//$(".r1").append(oou);
+return oou;
+}
+
+function tokana0()
+{var r='';
+for(x=0;x<reed;x++)
+{
+	if(reti[x]==0){r+=ret[x];}else{r+=reka[x];}
+}
+return r;}
+
+function rere(rrr)
+{ret0 = rrr;
+var in0=0;
+reed=0;
+ret[0]='';
+for(x=0, y=0;x<999;x++)//dilymo retwennya na frahmenty kanji/kana
+{	
+	if(ret0[x]>"");else{break;}
+	if(x==0){ if(ret0[x]>"ー"){reti[reed]=1;}else{reti[reed]=0;} }
+	else{
+		if(ret0[x]>"ー"){in0=1;}else{in0=0;}
+		if(reti[reed]!=in0){/*ret[reed]+='/';*/ reti[++reed]=in0; ret[reed]='';}
+		}
+	ret[reed]+=ret0[x];
+}reed++;
+
+for(x=0,p=0;x<reed;x++)//znaxodymo kana-vidpovidnyky do kanji
+{p=0;
+if(reti[x]==1)
+{
+	for(y=1;y<kss;y++)
+	{
+		if(ret[x]==mms[y][0][0]){reka[x]=mms[y][1][0]; p=1; break;}
+	}
+	if(p>0){continue;}
+	for(y=1;y<vss;y++)
+	{if(mmv[y][0]=='end'){break;}
+		if(ret[x]==mmv[y][0])
+		{
+			if(mmv[y][1]==0){reka[x]=mmv[y][2]; p=1; break;}
+			if(mmv[y][1]==ret[x+1][0]){reka[x]=mmv[y][2]; p=1; break;}
+		}
+	}
+	if(p==0){reka[x]='----';}
+	
+}
+	
+}
+
+//var kkk = tokana0();
+//wor=kkk;
+//atoa1( tokana0() );
+//return out1();
+return atoa1( tokana0() );
+}
+
+/*var main = function()
+{"use strict";
+
+$(".r1").append( atoa1("こんにちゐ") );
+$(".r1").append( "<p class='ua2'>Доброго дня!</p>" );
+$(".r1").append("<div class='ua3'>・Бути</div>");
+for(var x=0;x<3;x++)
+{
+//ret0 = retm[xx][0];
+//rere(retm[xx][0]);
+//$(".r1").append("<br>",ret,"<br>",reti,'<br>',reka);
+$(".r1").append( rere( retm[x][0] ) );
+$(".r1").append( "<p class='ua2'>"+retm[x][1]+"</p>" );
+}
+//$(".r1").append( atoa1("の") );
+$(".r1").append("<div class='ua3'>・の — стосунок</div>");
+for(var x=3;x<7;x++)
+{
+$(".r1").append( rere( retm[x][0] ) );
+$(".r1").append( "<p class='ua2'>"+retm[x][1]+"</p>" );
+}
+$(".r1").append("<div class='ua3'>・Дієслова</div>");
+for(var x=7;x<11;x++)
+{
+$(".r1").append( rere( retm[x][0] ) );
+$(".r1").append( "<p class='ua2'>"+retm[x][1]+"</p>" );
+}
+$(".r1").append("<div class='ua3'>・Питання</div>");
+$(".r1").append("<div class='ua3'>・Заперечення</div>");
+$(".r1").append("<div class='ua3'>・も — також</div>");
+$(".r1").append("<div class='ua3'>・Скорочення</div>");
+$(".r1").append("<div class='ua3'>・へ — до</div>");
+$(".r1").append("<div class='ua3'>・Пропозиція</div>");
+$(".r1").append("<div class='ua3'>・</div>");
+
+$(".lili").replaceWith('<section class="lili"></section>');
+var kkkk="た";
+var ccc= kkkk.charCodeAt(0);
+$(".lili").append(ccc);
+for(x=0;x<ska;x++)
+{
+	if(sps[x]=='　'){continue;}
+	if(sps[x]=='。'){continue;}
+	if(sps[x]=='だ'){continue;}
+	if(ski[x]>2){
+	$(".lili").append(sps[x],ski[x],", "); }
+}
+
+}
+
+$(document).ready(main);*/
---
+++ jmain.js
@@ -0,0 +1,186 @@
+var wn = 0;
+var tim = 0;
+var bul = 0;
+var ifa = 0;
+var np = 0;
+var a = new Audio();
+var bu = [0,0,0];
+var tip = 0;
+var ki0 = 0;
+
+function vyp(b,e){ return( Math.round(Math.random()*(e-b))+b); }
+
+function vsika(ky=1)
+{
+var gr=0;
+var e=0, res=mms[ky][0][e++];
+if(mms[ky][0][e-1]==1){e--;}
+if(mms[ky][0][0]=="["){gr=1; res="<span class='bl'>"; /*res+=mms[ky][0][e++];*/ }
+for(;mms[ky][0][e];e++)
+{
+if(mms[ky][0][e]=="]"){/*res+="]";*/ break;}
+if(mms[ky][0][e]=="("){
+	if(gr==1){res+="</span>";}else{gr=1;} res+="<span class='gr'>"; continue;}
+if(mms[ky][0][e]=="1"){e++;
+	res+="　";
+	if(e==1){res='';}
+	br0=0;
+	for(q=0;q<100;q++)
+	{	
+		if(mms[ky][0][e].charCodeAt(q)>0);else{break;}
+		if(mms[ky][0][e][q]=="("){br0=1; res+="<span class='re'>"; continue;}
+		if(mms[ky][0][e][q]==")" && br0==1){br0=0; res+="</span>"; continue;}
+		if(mms[ky][0][e][q]=="["){br0=1; res+="<span class='ge'>"; continue;}
+		if(mms[ky][0][e][q]=="]" && br0==1){br0=0; res+="</span>"; continue;}
+		res+=mms[ky][0][e][q];
+		//if(mms[ky][0][e].charCodeAt(q)>0);else{break;}
+	}
+continue;	
+}
+if(e==1 && mms[ky][0][0]=="["){res+=mms[ky][0][e];}else{res+="　"+mms[ky][0][e];}
+}e--;
+if(gr==1){res+="</span>";}
+return res;
+}
+
+function vypka(au0=0)
+{var e=0, mas1=['',''], res="ura";
+var if_=0;
+for(x=0;mms[wn][0][x];x++,e++)
+{
+if(mms[wn][0][x]=="["){if_=1; e--;continue;}
+if(mms[wn][0][x]=="("){break;}
+if(mms[wn][0][x]=="]"){/*e--;*/ break;}
+if(mms[wn][0][x]==1){ x++; //e--; continue;
+var sl3='';
+var br0=0;
+	for(q=0;q<100;q++)
+	{	
+		if(mms[wn][0][x].charCodeAt(q)>0);else{break;}
+		if(mms[wn][0][x][q]=="("){br0=1; continue;}
+		if(mms[wn][0][x][q]==")"){br0=0; continue;}
+		if(br0==1){continue;}
+		if(mms[wn][0][x][q]=="["){ continue;}
+		if(mms[wn][0][x][q]=="]"){ continue;}
+		sl3+=mms[wn][0][x][q];
+		//if(mms[wn][0][x].charCodeAt(q)>0);else{break;}
+	}
+mas1[e]=sl3; continue;
+}
+mas1[e]=mms[wn][0][x];
+}e--;
+
+if(au0==1){res=mas1[0];}else{res=mas1[ (vyp(0,e)) ];}
+//if(if_==1){res="<span class='bl'>"+res+"</span>";}
+//res=mas1;
+return res;
+}
+
+function vsit(ky=1)
+{
+var e=0;//, res='';
+if(mms[ky][1][e]==1){e++;}
+var res=mms[ky][1][e++];
+for(;mms[ky][1][e];e++)
+{
+if(mms[ky][1][e]==1){continue;}
+if(mms[ky][1][e]=="("){res+=" ("+mms[ky][1][++e]; continue;}
+res+="/"+mms[ky][1][e];
+}e--;
+//if( vyp(0,1) ){res=wanakana.toKatakana(res);}
+return res;
+}
+
+var allau=['',''], eau=0;
+function allauf()
+{eau=0;
+for(x=0;mms[wn][1][x];x++,eau++)
+{
+if(mms[wn][1][x]=="("){break;}
+if(mms[wn][1][x]==1){x++;eau--; continue;}
+allau[eau]=mms[wn][1][x];
+}eau--;
+}
+//var a = new Audio();
+function sou0(k)
+{var a = new Audio();
+//var akan=mms[wn][0][0]; if(akan=="["){akan=mms[wn][0][1];}
+var kan00=vypka(1);
+//$(".transl").append("SFSDF"+wn+kan00);
+a.src='http://assets.languagepod101.com/dictionary/japanese/audiomp3.php?kana='+allau[k]+'&kanji='+kan00;
+a.play();
+a.onended = function(){ if(k<eau){sou0(++k);} }
+}
+
+function sou(vo)
+{
+allauf();
+if(eau>-1){ sou0(0); }
+//$(".transl").append("SFSDF"+wn);
+}
+var kann='';
+function word(){
+var bubu=0;
+	if(bul==0){bul=1; //np=vyp(0,1);
+	np=0;
+	tim++; tip++;
+	if(tip>kss-2){tip=0;}
+	//wn = (vyp(0,kss))+1;
+	///////////////
+	for(x=0;x<100;x++)
+	{
+	wn = vyp( 1,(kss-1) );
+	//wn = (vyp(3200,kss))+1;
+	//if(vp[wn][np+5]<2.1){continue;}
+	if(vp[wn][np+5]>2){continue;}
+		for(y=1;y<tip;y++){	if(wn==bu[y]){bubu=1; ki0++; break;}	}
+		if(bubu){bubu=0; continue;}
+	bu[tip]=wn;
+	break;
+	}
+	////////////////
+	$(".time").replaceWith("<p class='time'>"+tim+"("+ki0+")"+"</p>");
+	if(tim<2){$(".time").append(" "+kss);}
+	$(".num").replaceWith("<p class='num'>"+wn+" : "+vp[wn][np+5]+"</p>");
+	$(".transc").replaceWith("<p class='transc'> </p>");
+		if(np==0){
+		$(".orig").replaceWith("<p class='orig'>"+vypka(wn)+"</p>");
+		$(".transl").replaceWith("<p class='transl'> </p>");
+		//$(".transc").replaceWith("<p class='transc'>"+atoa1(mms[wn][1])+"</p>");
+		//kann=atoa1(mms[wn][1][0]);
+		$(".transc").replaceWith("<div class='transc'>");
+		//$(".transc").append( "<div class='ccc'>//</div>" );
+		//$(".transc").append( vsit(wn) ,"</p>" );
+		$(".transc").append( atoa1( vsit(wn) ),"</div>" );
+		if(ifa){ sou(); }
+		}
+		else{
+		$(".orig").replaceWith("<p class='orig'> </p>");
+		$(".transl").replaceWith("<p class='transl'>"+mms[wn][2]+"</p>");
+		}
+	}else{bul=0;
+	//$(".transc").replaceWith("<p class='transc'>"+vsit(wn)+"</p>");
+	$(".orig").replaceWith("<p class='orig'>"+vsika(wn)+"</p>");
+		if(np==0){
+		$(".transl").replaceWith("<p class='transl'>"+mms[wn][2]+"</p>");
+		}
+		else{
+		//$(".orig").replaceWith("<p class='orig'>"+vsika(wn)+"</p>");
+		}
+	//if(ifa){ sou(); }
+	}
+}
+
+var main = function() {
+"use strict";
+
+word();
+
+$("button.bm").on("click", function(event){ word(); });
+$("button.bsou").on("click", function(event){
+	if(ifa==0){ifa=1; document.getElementById("bs").style.color = "blue"; sou();
+	}else{ifa=0; document.getElementById("bs").style.color = "red"; a.pause(); a.currentTime = 0.0;}
+});
+
+};
+$(document).ready(main);
