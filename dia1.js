8eb2280eaf6a68b12f084f69be035afcf05a97c7
Sultepronit <65405818+Sultepronit@users.noreply.github.com>
2021-03-01 21:29:30 +0200

    Add files via upload

---
+++ dia1.js
@@ -0,0 +1,16 @@
+
+
+var main = function()
+{"use strict";
+
+$(".d1").append( "<p class='ua2'><i>Ямада-сан і Танака-сан зустрілись в університеті на святі.</i></p>" );	
+for(var x=42;x<56;x++)
+{	$(".d1").append( rere( retm[x][0] ) );
+	$(".d1").append( "<p class='ua2'>"+retm[x][1]+"</p>" );	}
+
+
+$(".lili").replaceWith('<section class="lili"></section>');
+
+}
+
+$(document).ready(main);
